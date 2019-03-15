1da70a3bdc9a53b6d7af6fdd33945bbea0c11f5b
Caobin <caobin@CaoBin-1.local>
2019-03-15 14:21:14 +0800

    data_maker

--- 01XOR/train.py
+++ 01XOR/train.py
@@ -19,19 +19,31 @@ net = Net()
 print(net)
 
 learning_rate = 0.1
+
+def xor(a, b):
+    if a*b > 0:
+        return 1
+    else
+        return -1
+
+def data_provider():
+    data_in = torch.randn(100,2)
+    label = torch.Tensor(100,1)
+    for i in range(100):
+        label[i][0] = xor(data_in[i][0], data_in[i][1])
+    return data_in, label
+
 while True:
-    data_in = torch.randn(2)
-    data_in = data_in.view(1, -1)
+    data_in, gt = data_provider()
     data_out = net(data_in)
-    gt = torch.Tensor(1)
-#    print(data_in)
+    # print(data_in)
     gt[0] = data_in[0][0] * data_in[0][1]
     if gt[0] > 0:
         gt[0] = 1
     else:
         gt[0] = 0
     gt = gt.view(1, -1)
-#    print(gt)
+    # print(gt)
     criterion = nn.L1Loss()
     loss = criterion(data_out, gt)
     net.zero_grad()
