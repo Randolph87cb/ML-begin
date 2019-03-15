c9c8ab50c953a828720d37311cd696adba2dfc20
Caobin <caobin@CaoBin-1.local>
2019-03-15 13:17:26 +0800

    train and test

--- 01XOR/train.py
+++ 01XOR/train.py
@@ -17,3 +17,31 @@ class Net(nn.Module):
 
 net = Net()
 print(net)
+
+learning_rate = 0.1
+while True:
+    data_in = torch.randn(2)
+    data_in = data_in.view(1, -1)
+    data_out = net(data_in)
+    gt = torch.Tensor(1)
+    gt[0] = data_in[0][0] * data_in[0][1]
+    if gt[0] > 0:
+        gt[0] = 1
+    else:
+        gt[0] = 0
+    gt = gt.view(1, -1)
+    loss = criterion(data_out, gt)
+    net.zero_grad()
+    loss.backward()
+    for f in net.parameters():
+        f.data.sub_(f.grad.data * learning_rate)
+    learning_rate = learning_rate * 0.9
+    if learning_rate < 0.00001:
+        break
+
+data_in = torch.randn(2)
+data_in = data_in.view(1, -1)
+data_out = net(data_in)
+
+print(data_in)
+print(data_out)
