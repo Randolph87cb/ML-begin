ebdf7be1a7435204627687b9615ca7f4ac10c20b
Caobin <caobin@CaoBin-1.local>
2019-03-15 12:57:46 +0800

    get net

---
+++ 01XOR/train.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(2, 4)
+        self.fc2 = nn.Linear(4, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+net = Net()
+print(net)
